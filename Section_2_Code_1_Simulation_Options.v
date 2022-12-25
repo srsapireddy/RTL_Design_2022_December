739f84751711a85e7b6e4092af985c4b8d797bfc
Srinivas Rahul <32967087+srsapireddy@users.noreply.github.com>
2022-12-24 18:04:01 -0600

    Section_2_Simulation_Options_December_2022_24th

---
+++ Section_2_Code_1_Simulation_Options.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+ 
+module top(
+    input [3:0] a,b,
+    output [4:0] y
+    );
+ 
+reg [4:0] temp;
+ 
+always@(*)
+begin
+temp = a + b;
+end    
+assign y = temp;  
+    
+  
+endmodule
+ 
+/////////////////////////////////
+ 
+module tb;
+ 
+reg [3:0] a = 0, b = 0;
+wire [4:0] y;
+ 
+top dut (a, b, y); ///implicit 
+ 
+integer i = 0;
+initial begin
+for(i = 0; i< 20; i = i+1) begin
+a = $urandom;
+b = $urandom;
+#10;
+end
+end
+ 
+ 
+endmodule
